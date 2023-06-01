320b8757f5c1a9c630e73efffd68014a8559d403
wilsongumba <wilsongumba@gmail.com>
2023-06-02 01:32:00 +0300

    add failing test and improve reporting

--- playwright.config.js
+++ playwright.config.js
@@ -17,18 +17,24 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    [process.env.CI ? "github" : "list"],
+    ["html", { outputFolder: "./playwright-report/playwright-html-report", open: "never" }],
+    ["junit", { outputFile: "./playwright-report/results.xml" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
--- tests/chapterTwo.test.js
+++ tests/chapterTwo.test.js
@@ -7,3 +7,10 @@ test('has title google', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Google/);
 });
+
+test('should fail', async ({ page }) => {
+	await page.goto('http://google.com');
+  
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Ggle/);
+  });
