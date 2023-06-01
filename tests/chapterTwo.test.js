42fb03857a0b7aa630c05750c24ef1a4fd9af7c1
wilsongumba <wilsongumba@gmail.com>
2023-06-02 01:51:28 +0300

    remove failing test, add more commands

--- tests/chapterTwo.test.js
+++ tests/chapterTwo.test.js
@@ -7,10 +7,3 @@ test('has title google', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Google/);
 });
-
-test('should fail', async ({ page }) => {
-	await page.goto('http://google.com');
-  
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/Ggle/);
-  });
